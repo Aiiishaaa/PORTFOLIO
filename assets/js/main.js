e7d6392a8a79693f6a6bec604c48807aaee9b481
Aiiishaaa <aicha.hamida06@yahoo.fr>
2021-07-31 17:40:21 +0200

    update CV

--- assets/js/main.js
+++ assets/js/main.js
@@ -140,6 +140,7 @@
         });
     }
 
+
     // Porfolio isotope and filter
     $(window).on('load', function() {
         var portfolioIsotope = $('.portfolio-container').isotope({
@@ -147,7 +148,7 @@
         });
 
         $('#portfolio-flters li').on('click', function() {
-            $("#portfolio-flters li").removClases('filter-active');
+            $("#portfolio-flters li").removeClass('filter-active');
             $(this).addClass('filter-active');
 
             portfolioIsotope.isotope({
